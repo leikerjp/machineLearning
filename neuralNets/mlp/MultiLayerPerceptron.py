ca3b162d1497bd93e13668031d581bbcef11904b
Jordan Leiker <jpleiker@gmail.com>
2020-12-30 15:07:04 -0500

    Added feedforward method to MLP class. Tested, is working for no hidden layers and multiple hidden layer cases.

--- neuralNets/mlp/MultiLayerPerceptron.py
+++ neuralNets/mlp/MultiLayerPerceptron.py
@@ -16,67 +16,64 @@ class MultiLayerPerceptron(object):
         self.inputSize = int(inputSize)
         self.numHiddenLayers = numHiddenLayers
         if self.numHiddenLayers > 0:
-            if (len(self.neuronsByLayer) != self.numHiddenLayers):
+            if (len(numNeuronByHiddenLayer) != self.numHiddenLayers):
                 print("ERROR - input parameter numHiddenLayers must be the length of list numNeuronsByHiddenLayer")
                 exit
             else:
-                self.neuronsByLayer = numNeuronByHiddenLayer
+                self.numNeuronByHiddenLayer = numNeuronByHiddenLayer
         self.activation = activationFunction
         self.outputSize = int(outputSize)
         self.trainingIterations = trainingIterations
         
         # Neurel Net Setup
         ## NN has a list member for each layer, and each layer has all neurons stored in numpy array
-        ## Weights / Neurons stored as:
+        ## Weights / Layer stored as:
         #### rows are weights per neuron
         #### columns are neurons per layer
         #### | w1_n1   w1_n2  ...  w1_nN | 
         #### | w2_n1   w2_n2  ...  w2_nN |
         #### | w3_n1   w3_n2  ...  w3_nN |
+        ## bias / Layer stored as:
+        #### | b_n1    b_n2   ...  b_nN  |
         if self.numHiddenLayers > 0: 
             ### layer with input connections
             self.weightsByLayer = [np.random.random((self.inputSize, self.numNeuronByHiddenLayer[0]))]
+            self.biasByLayer = [np.random.random((1, self.numNeuronByHiddenLayer[0]))]
             ### all hidden layers
             for ii in range(numHiddenLayers-1):
                 self.weightsByLayer.append(np.random.random((self.numNeuronByHiddenLayer[ii], self.numNeuronByHiddenLayer[ii+1]))) # neurons are initialized at random for now
+                self.biasByLayer.append(np.random.random((1, self.numNeuronByHiddenLayer[ii+1])))
             ### output layer
-            self.weightsByLayer.append(np.random.random((self.outputSize, self.numNeuronByHiddenLayer[-1])))
+            self.weightsByLayer.append(np.random.random((self.numNeuronByHiddenLayer[-1], self.outputSize)))
+            self.biasByLayer.append(np.random.random((1, self.outputSize)))
         else:
             ### single layer
             self.weightsByLayer = [np.random.random((self.inputSize, self.outputSize))]
+            self.biasByLayer = [np.random.random((1, self.outputSize))]
 
-    def train(self, data):
+
+    """Return the networks output given an input, iData"""
+    def feedforward(self, data):
+
         # Error check input data size
-        if self.numHiddenLayers > 0: 
-            if data.shape[1] != self.weightsByLayer[0].shape[0]:
-                print("ERROR - data size must be equal to inputSize")
-                exit()
+        if data.shape[1] != self.weightsByLayer[0].shape[0]:
+            print("ERROR - data size must be equal to inputSize")
+            exit()
 
-            # Training iterations
-            for iter in range(self.trainingIterations):
-                # first layer
-                weights = self.activation(data @ self.weightsByLayer[0]) 
-                # the rest of the layers (if they exist)
-                for ii in range(len(self.weightsByLayer) - 1):
-                    weights = self.activation(weights @ self.weightsByLayer[ii+1])
+        # Iterate through each layer
+        for weight, bias in zip(self.weightsByLayer, self.biasByLayer):
+            data = self.activation(data @ weight + bias) 
 
-        else:
-            if data.shape[1] != self.weightsByLayer.shape[0]:
-                print("ERROR - data size must be equal to inputSize")
-                exit()
-
-            # Training iterations
-            for iter in range(self.trainingIterations):
-                # only layer
-                print(data)
-                print(self.weightsByLayer)
-                weights = self.activation(data @ self.weightsByLayer) 
+        return data
 
 
-        return weights
+    def train(self, iData):
+        # Training iterations
+        for iter in range(self.trainingIterations):
+            print("nothing yet")
 
 
-    def predict(self, data):
+    def predict(self, iData):
         pass
 
 
--- neuralNets/mlp/test.py
+++ neuralNets/mlp/test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.matlib
 import MultiLayerPerceptron as mlp
 from activations import relu, sigmoid
 
@@ -6,6 +7,7 @@ np.random.seed(1)
 
 testMLP = mlp.MultiLayerPerceptron(inputSize = 3,
                                    numHiddenLayers = 0,
+                                   numNeuronByHiddenLayer = [2],
                                    activationFunction = sigmoid,
                                    outputSize = 1,
                                    trainingIterations = 1)
@@ -21,9 +23,9 @@ Y = np.array([[0,1,1,0]]).T
 
 
 print(testMLP.weightsByLayer)
-testMLP.weightsByLayer = 2 * testMLP.weightsByLayer[0] - 1 
-print(testMLP.weightsByLayer)
+testMLP.weightsByLayer[0] = 2 * testMLP.weightsByLayer[0] - 1
+testMLP.biasByLayer[0] = 0 
 
 
-Z = testMLP.train(X)
+Z = testMLP.feedforward(X)
 print(Z)
